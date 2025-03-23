c6d3083d4bdb0fe7ee0f1d0d8ae219f0e0fabd7d
Harshal Bokade <harshalbokade984@gmail.com>
2025-03-23 20:32:12 +0530

    updated the component use in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import moment from 'moment';
+import DateRangePicker from './DateRangePicker';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ranges = {
+    Today: [moment(), moment()],
+    Yesterday: [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+    'This Month': [moment().startOf('month'), moment().endOf('month')],
+  };
+  const [dateRange, setDateRange] = useState({
+    startDate: moment().subtract(10, 'days'),
+  });
+
+  // Define custom options
+  const options = {
+    singleDatePicker: false,
+    showFullDateRangeLabel: true,
+    // icon: <Icon name="calender" />,
+    showInputField: false,
+    showDropdowns: true,
+    timePicker: true,
+    timePicker24Hour: false,
+    timePickerIncrement: 15,
+    alwaysShowCalendars: false,
+    linkedCalendars: false,
+    autoApply: false,
+    showRanges: true,
+    opens: 'left',
+    drops: 'down',
+    locale: {
+      format: 'MM/DD/YYYY h:mm A',
+      separator: ' - ',
+      applyLabel: 'Apply',
+      cancelLabel: 'Cancel',
+      customRangeLabel: 'Custom Range',
+    },
+  };
+
+  const handleApply = (dateRange) => {
+    console.warn('date range previous state:', dateRange);
+    console.warn('formatted date range:', {
+      startDate: dateRange.startDate.format('MM/DD/YYYY h:mm A'),
+      endDate: dateRange.endDate.format('MM/DD/YYYY h:mm A'),
+    });
+    console.warn('date range onApply', dateRange);
+    setDateRange(dateRange);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ width: '100%' }}>
+        <DateRangePicker
+          options={options}
+          ranges={ranges}
+          onApply={handleApply}
+          startDate={dateRange?.startDate}
+          endDate={dateRange?.endDate}
+          placeholder="Select date range"
+          timeZone="America/New_York"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
