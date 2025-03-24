4ba5b3773993e5cd902295de522df2c1f2a09438
Harshal Bokade <harshalbokade984@gmail.com>
2025-03-24 14:42:39 +0530

    optimize app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,49 @@
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import moment from 'moment';
 import DateRangePicker from '.';
 
 function App() {
-  const ranges = {
-    Today: [moment(), moment()],
-    Yesterday: [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-    'This Month': [moment().startOf('month'), moment().endOf('month')],
-  };
-  const [dateRange, setDateRange] = useState({
-    startDate: moment().subtract(10, 'days'),
-  });
+  const ranges = useMemo(
+    () => ({
+      Today: [moment(), moment()],
+      Yesterday: [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+      'Last Minute': [moment().subtract(1, 'minutes'), moment()],
+      'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+      'This Month': [moment().startOf('month'), moment().endOf('month')],
+    }),
+    []
+  );
+  const [dateRange, setDateRange] = useState({});
 
   // Define custom options
-  const options = {
-    singleDatePicker: false,
-    showFullDateRangeLabel: true,
-    // icon: <Icon name="calender" />,
-    showInputField: false,
-    showDropdowns: true,
-    timePicker: true,
-    timePicker24Hour: false,
-    timePickerIncrement: 15,
-    alwaysShowCalendars: false,
-    linkedCalendars: false,
-    autoApply: false,
-    showRanges: true,
-    opens: 'left',
-    drops: 'down',
-    locale: {
-      format: 'MM/DD/YYYY h:mm A',
-      separator: ' - ',
-      applyLabel: 'Apply',
-      cancelLabel: 'Cancel',
-      customRangeLabel: 'Custom Range',
-    },
-  };
+  const options = useMemo(() => {
+    return {
+      // singleDatePicker: true,
+      showFullDateRangeLabel: true,
+      // icon: <Icon name="calender" />,
+      showInputField: false,
+      showDropdowns: true,
+      timePicker: true,
+      timePicker24Hour: false,
+      timePickerIncrement: 15,
+      alwaysShowCalendars: false,
+      linkedCalendars: false,
+      autoApply: false,
+      showRanges: true,
+      opens: 'left',
+      drops: 'down',
+      locale: {
+        format: 'MM/DD/YYYY h:mm A',
+        separator: ' - ',
+        applyLabel: 'Apply',
+        cancelLabel: 'Cancel',
+        customRangeLabel: 'Custom Range',
+      },
+    };
+  }, []);
 
-  const handleApply = (dateRange) => {
+  const handleApply = useCallback((dateRange) => {
     console.warn('date range previous state:', dateRange);
     console.warn('formatted date range:', {
       startDate: dateRange.startDate.format('MM/DD/YYYY h:mm A'),
@@ -47,7 +51,7 @@ function App() {
     });
     console.warn('date range onApply', dateRange);
     setDateRange(dateRange);
-  };
+  }, []);
 
   return (
     <>
